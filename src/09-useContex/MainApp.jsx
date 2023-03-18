7b02813ffca4ad7407fad06128aa5ad936efa39b
tecnoboyer <ingenieroboyer@gmail.com>
2023-03-18 11:19:51 -0400

    creando componente del Navbar que solo maneja html y no requiere entonces pase de parametros desde el componente padre

--- src/09-useContex/MainApp.jsx
+++ src/09-useContex/MainApp.jsx
@@ -2,14 +2,14 @@ import { Link, Navigate, Route, Routes } from "react-router-dom"
 import { AboutPage } from "./AboutPage"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
+import { Navbar } from "./Navbar"
 
 
 export const MainApp = () => {
   return (
     <>
     <h1>MainApp</h1>
-    <Link to= "/about">About </Link>
-    <Link to="/login">Login </Link>
+    < Navbar />
 
 
 
