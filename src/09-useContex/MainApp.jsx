ad335d5987c66458cf479f378ab95b29c41861d8
tecnoboyer <ingenieroboyer@gmail.com>
2023-03-18 11:13:46 -0400

    usuando link para navegar

--- src/09-useContex/MainApp.jsx
+++ src/09-useContex/MainApp.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom"
+import { Link, Navigate, Route, Routes } from "react-router-dom"
 import { AboutPage } from "./AboutPage"
 import { HomePage } from "./HomePage"
 import { LoginPage } from "./LoginPage"
@@ -8,12 +8,20 @@ export const MainApp = () => {
   return (
     <>
     <h1>MainApp</h1>
+    <Link to= "/about">About </Link>
+    <Link to="/login">Login </Link>
+
+
+
     <hr />
     
     <Routes>
         <Route path="/" element={< HomePage />} />
         <Route path="/login" element={< LoginPage />} />
         <Route path="/about" element={< AboutPage />} />
+
+        {/* <Route path="/*" element={ < AboutPage/> } /> */}
+        <Route path="/*" element={ < Navigate to="/about" /> } />
 
       </Routes>
 
